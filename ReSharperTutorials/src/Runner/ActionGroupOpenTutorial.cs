10493226d74bbba9c13f5d612a784c174ae454b4
DarthWeirdo <atot1981@gmail.com>
2016-06-30 14:24:03 +0300

    rebuild for wave06

---
+++ ReSharperTutorials/src/Runner/ActionGroupOpenTutorial.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.UI.ActionsRevised;
+using JetBrains.UI.MenuGroups;
+
+namespace ReSharperTutorials.Runner
+{
+    [ActionGroup("ActionGroupOpenTutorial", ActionGroupInsertStyles.Submenu, Text = "Tutorials", Id = 200)]
+    public class ActionGroupOpenTutorial : IAction, IInsertLast<MainMenuFeaturesGroup>
+    {
+        public ActionGroupOpenTutorial(ActionOpenTutorial1 action)
+        {            
+        }                
+    }
+}
